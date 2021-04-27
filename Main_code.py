d6f1f831919705872e8b5f43b4c808b38810f9cd
uvsq22004316 <71564594+uvsq22004316@users.noreply.github.com>
2021-04-28 00:12:11 +0200

    Update Main_code.py

--- Main_code.py
+++ Main_code.py
@@ -22,6 +22,9 @@ from tkinter import font as tkFont
 
 HEIGHT = 800
 WIDTH = 1200
+
+SCORE_H = 30
+SCORE_W = 100
 #helv26 = tkFont.Font(family='Comic Sans MS', size=16, weight='bold')
 #fontStyle = tkFont.Font(family="Comic sans MS", size=20)
 #fontStyle2 = tkFont.Font(family="Comic sans MS", size=10)
@@ -64,18 +67,40 @@ def P1j():
 
 def P2j():
     menu.destroy()
+    
+def regles():
+    """fonction liée au bouton 'Règles du jeu'. Ouvre une fenêtre auxiliaire
+    avec explication des règles. Quand on appuie sur le bouton 'Retour', la fenêtre
+    se ferme et on revient au menu"""
+    rules = tk.Toplevel(menu)
+    rules.title("Règles du jeu")
+    regles = tk.Canvas(rules, height=400, width=1000, bg='white')
+    def close():
+        rules.destroy()
+    retour = tk.Button(rules, text='Retour', command=close)
+    regles.create_text(500, 200, text="Chacun des deux joueurs "
+                   "dispose de 3 jetons.\nUne fois tous les pions posés,\n"
+                   "chaque joueur peut à son tour déplacer un pion d'une case\n horizontale,"
+                   "verticale ou diagonale\nLe vainqueur est le premier joueur à\naligner"
+                   " 3 de ses jetons sur le plateau.", font=('helvetica','14'))
+    regles.create_text(540, 20, text='RÈGLES DU JEU DU TAPATAN', font=('helvetica','20'))
+    regles.grid(row=0)
+    retour.grid(row=1)
 
 bck = tk.Canvas(menu, bg = "red", height = 150, width = 400)
 titre = tk.Label(menu, text="Jeu Du Tapatan Win97")
 buttonII = tk.Button(menu, text="0 Joueurs", command = P0j)
 buttonHH = tk.Button(menu, text="1 Joueur", command = P1j)
 buttonHI = tk.Button(menu, text="2 Joueurs", command = P2j)
+button_rules = tk.Button(menu, text='Règles du jeu', command=regles)
+
 
 bck.grid(column = 0, row = 0, columnspan = 3,padx = 300)
 titre.grid(column = 0, row = 0, columnspan = 3,padx = 300)
 buttonII.grid(column = 0, row = 1, padx = 500, pady = 30)
 buttonHH.grid(column = 0, row = 2, pady = 30, )
 buttonHI.grid(column = 0, row = 3, pady = 30)
+button_rules.grid(column=0, row=4, pady=30)
 
 
 menu.mainloop()
@@ -85,18 +110,22 @@ menu.mainloop()
 
 plateau = tk.Tk()
 
+r=0
+b=0
+
 canvas = tk.Canvas(plateau, height=HEIGHT, width=WIDTH)
 
 bouton_sauvegarder = tk.Button(plateau, text='Sauvegarder la partie',
                                command=sauvegarder)
 bouton_recharger = tk.Button(plateau, text='Recharger la partie', command=recharger)
 
-#score = tk.Label(plateau, text=('SCORE : ',r,' - ',b))
+score = tk.Canvas(plateau, height=60, width=300, bg='dark khaki')
 
-#score.grid(row=0, columnspan=2)
+score.create_text(SCORE_W, SCORE_H, text='SCORE : ', font=('helvetica', '16'))
+score_rouge = score.create_text(SCORE_W+80, SCORE_H, text=r, font=('helvetica', '16'))
+score.create_text(SCORE_W+100, SCORE_H, text='-', font=('helvetica', '16'))
+score_bleu = score.create_text(SCORE_W+120, SCORE_H, text=b, font=('helvetica', '16'))
 
-r=0
-b=0
 
 #digit1 = tk.Label(plateau, text="0", width = 5, height = 5)
 #digit2 = tk.Label(plateau, text="-", width = 30, height = 5)
@@ -107,7 +136,8 @@ b=0
 
 bouton_sauvegarder.grid(row=3, column=0)
 bouton_recharger.grid(row=3, column=1)
-canvas.grid(row=0, columnspan=2)
+canvas.grid(row=1, columnspan=2)
+score.grid(row=0, columnspan=2)
 
 rouge1 = canvas.create_oval((50, 500), (100, 550), fill="red", outline="red")
 rouge2 = canvas.create_oval((50, 600), (100, 650), fill="red", outline="red")
@@ -164,6 +194,72 @@ def dePion(event):
 
             if x <= x2 and y <= y2 and x >= x1 and y >= y1:
                 canvas.itemconfig(liCe[i][j], fill = 'blue')
+                matrice[i][j] = 2 #j'ai rajouté ça là pour l'instant (NK)
+    won_ckeck(matrice) #je l'ai associé ici, à voir (NK)
+    
+def won_ckeck(matrice):
+    '''évalue après chaque tour si qqn a gagné. s'il y a un gagnant, un msg 
+    s'affiche dans une nouvelle fenêtre. Lance une nouvelle partie'''
+    global r, b
+    for i in range(3):
+        if matrice[i][0] == matrice[i][1] == matrice[i][2] == 1:
+            print( "Joueur 1 a gagné!!!!!!")
+            r += 1
+            fin_de_partie()
+        elif matrice[i][0] == matrice[i][1] == matrice[i][2] == 2:
+            msg_gagne()
+            b += 1
+            score.itemconfigure(score_bleu, text=b)
+            fin_de_partie()
+    for j in range(3):
+        if  matrice[0][j] == matrice[1][j] == matrice[2][j] == 1:
+            print( "Joueur 1 a gagné!!!!!!")
+            r += 1
+            fin_de_partie()
+        elif matrice[0][j] == matrice[1][j] == matrice[2][j] == 2:
+            msg_gagne() 
+            b += 1
+            score.itemconfigure(score_bleu, text=b)
+            fin_de_partie()
+    if matrice[0][0] ==  matrice[1][1] == matrice[2][2]:
+        if matrice[0][0] == 1:
+            print( "Joueur 1 a gagné!!!!!!")
+            r += 1
+            fin_de_partie()
+        elif matrice[0][0] == 2:
+            msg_gagne()
+            b += 1
+            score.itemconfigure(score_bleu, text=b)
+            fin_de_partie()
+    if matrice[2][0] ==  matrice[1][1] == matrice[0][2]:
+        if matrice[i][j] == 1:
+            print( "Joueur 1 a gagné!!!!!!")
+            r += 1
+            fin_de_partie()
+        elif matrice[2][0] == 2:
+            msg_gagne()
+            b += 1
+            score.itemconfigure(score_bleu, text=b)
+            fin_de_partie()
+
+
+def msg_gagne():
+    """"fenetre auxiliaire qui affiche message 'Gagné' par dessus le plateau"""
+    msg = tk.Toplevel(plateau)
+    msg.title("Fin de partie")
+    gagné = tk.Canvas(msg, height=100, width=400, bg='dark khaki')
+    gagné.create_text(130, 60, text='Joueur', font=('helvetica', '16'))
+    gagné.create_text(190, 60, text='2', font=('helvetica', '16'))
+    gagné.create_text(280, 60, text='a gagné !', font=('helvetica', '16'))
+    gagné.grid()
+    #msg.after(3000, msg.destroy()) # ça marche pas????
+
+
+def fin_de_partie():
+    """relance une partie tant qu'il n'y a pas de vainqueur"""
+    #global r, b
+    #while r < 4 or b < 4:
+    pass
 
 
 canvas.bind("<Button-1>", dePion)
